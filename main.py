8c69397f4b75a31785f4d646875e461613d2623c
Aleksandr Belyaev <belas80@gmail.com>
2023-08-05 22:57:53 +0500

    done lesson 6

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class Animals:
+class Animal:
 
     def __init__(self, name, weight):
         self.name = name
@@ -8,41 +8,92 @@ class Animals:
         print(f'Покормили {self.name}')
 
 
-class Gooses(Animals):
+class Bird:
 
-    def gather_eggs(self):
-        print(f'Собираем яйца у {self.name}')
-
-
-class Cows:
-    name = ''
-    weight = 0
+    def get_eggs(self):
+        print(f'Собрали яйца')
 
 
-class Sheep:
-    name = ''
-    weight = 0
+class Milk:
+
+    def get_milk(self):
+        print(f'Подоили')
 
 
-class Chickens:
-    name = ''
-    weight = 0
+class Goose(Animal, Bird):
+
+    type_animal = 'гусь'
+
+    def say(self):
+        print('Га Га Га')
 
 
-class Goats:
-    name = ''
-    weight = 0
+class Cow(Animal, Milk):
+
+    type_animal = 'корова'
+
+    def say(self):
+        print('Муууу')
 
 
-class Ducks:
-    name = ''
-    weight = 0
+class Sheep(Animal):
+
+    type_animal = 'овца'
+
+    def say(self):
+        print('Беее')
+
+    def cut(self):
+        print(f'Подстригли {self.type_animal} {self.name}')
+
+
+class Chicken(Animal, Bird):
+
+    type_animal = 'курица'
+    def say(self):
+        print('Ко Ко Ко')
+
+
+class Goat(Animal, Milk):
+
+    type_animal = 'коза'
+
+    def say(self):
+        print('Мееее')
+
+
+class Duck(Animal, Bird):
+
+    type_animal = 'утка'
+
+    def say(self):
+        print('Кря Кря Кря')
 
 
 if __name__ == '__main__':
-    goose_1 = Gooses('Серый', '1')
-    goose_1.gather_eggs()
-    goose_1.feed()
-    goose_2 = Gooses(name='Белый', weight='2')
-    print(f'Вес у гуся {goose_2.name} - {goose_2.weight}')
-    goose_2.feed()
+    goose_1 = Goose('Серый', 1)
+    goose_2 = Goose(name='Белый', weight=2)
+    cow_1 = Cow(name='Манька', weight=15)
+    sheep_1 = Sheep('Барашек', 5)
+    sheep_2 = Sheep('Кудрявый', 4)
+    chicken_1 = Chicken('Ко-Ко', 1)
+    chicken_2 = Chicken('Кукареку', 1)
+    goat_1 = Goat('Рога', 7)
+    goat_2 = Goat('Копыта', 8)
+    duck_1 = Duck('Кряква', 3)
+
+    list_animals = [goose_1, goose_2, cow_1, sheep_1, sheep_2,
+                    chicken_1, chicken_2, goat_1, goat_2, duck_1]
+
+    sum_weight = 0
+    max_weight = 0
+    max_animal = ''
+
+    for i in list_animals:
+        sum_weight += i.weight
+        if i.weight > max_weight:
+            max_weight = i.weight
+            max_animal = i
+
+    print(f'Общий вес животных - {sum_weight}')
+    print(f'Самое тяжелое животное - {max_animal.type_animal} {max_animal.name}')
