0c95174bfd30e7ccfda16238471b5a2dce7366a1
Aleksandr Belyaev <belas80@gmail.com>
2023-09-08 22:47:38 +0500

    Done task 1 of lesson7

--- lesson7.py
+++ lesson7.py
@@ -1,20 +1,13 @@
 import json
 
 with open('recipes_lesson7.txt', 'r') as f:
-    result = f.read()
+    list_book = []
+    for i in f.read().split(sep='\n\n'):
+        list_book.append(i.split('\n'))
 
-result  = result.split(sep='\n\n')
-result2 = []
-for i in result:
-    result2.append(i.split('\n'))
-result = result2
-# list_keys = ['ingredient_name', 'quantity', 'measure']
-# cook_book = [dict(zip(list_keys, i.split(' | '))) for i in result[2:2 + int(result[1])]]
-# cook_book = {result[0]: cook_book}
 list_keys = ['ingredient_name', 'quantity', 'measure']
 cook_book = {}
-for line in result:
+for line in list_book:
     cook_book[line[0]] = [dict(zip(list_keys, i.split(' | '))) for i in line[2:2 + int(line[1])]]
 
-# print(cook_book)
 print(json.dumps(cook_book, ensure_ascii=False, indent=4))
