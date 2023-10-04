f2bdc3a49ed216a3ee6597d52933f9dacbaa59ac
Aleksandr Belyaev <belas80@gmail.com>
2023-10-05 01:12:39 +0500

    done task 2 of lesson9

---
+++ lesson9/task_2.py
@@ -0,0 +1,30 @@
+import requests
+from pprint import pprint
+
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def upload(self, file_path: str):
+        """Метод загруджает файл file_path на яндекс диск"""
+        headers = {'Authorization': 'OAuth ' + self.token}
+        params = {'path': '/' + file_path, 'overwrite': 'true'}
+        resp_geturl = requests.get(
+            'https://cloud-api.yandex.net/v1/disk/resources/upload',
+            params=params,
+            headers=headers
+        )
+        resp_geturl.raise_for_status()
+        href = resp_geturl.json()['href']
+        with open(file_path, 'rb') as f:
+            response = requests.put(href, files={"file": f})
+            response.raise_for_status()
+        return response
+
+
+if __name__ == '__main__':
+    with open('token', 'r') as t:
+        my_token = t.read()
+    uploader = YaUploader(my_token)
+    result = uploader.upload('file_for_upload.png')
